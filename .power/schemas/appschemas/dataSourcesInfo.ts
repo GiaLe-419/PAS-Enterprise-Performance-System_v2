f2a7121fdf82078914f86f40cbd35ef88d1ba967
Giang <duonghaigiang127@gmail.com>
2026-06-16 20:37:39 +0700

    Add dataverse

---
+++ .power/schemas/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "new_appraisalcycle1s": {
+    "tableId": "",
+    "version": "",
+    "primaryKey": "new_appraisalcycle1id",
+    "dataSourceType": "Dataverse",
+    "apis": {}
+  }
+};
